e4795c594c54240cc311f75b2b5bccafbd12704c
alexandra spalato <alexaspalato@gmail.com>
2022-07-24 11:43:02 +0200

    initial commit

---
+++ .slicemachine/assets/slices/Intro/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Intro';
+
+export default {
+  title: 'slices/Intro'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"intro","items":[],"primary":{"title":[{"type":"heading1","text":"Brand impactful experiences","spans":[]}],"description":[{"type":"paragraph","text":"Magna ut anim excepteur eiusmod ipsum officia laborum in dolore dolore consectetur nostrud voluptate. Commodo voluptate ea nulla culpa excepteur laboris Lorem.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
